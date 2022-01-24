2f5bf2f082aad61e6530f7672cd290eda6ed1d63
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-01-24 14:25:19 +0000

    Beginning Python!

---
+++ Python/Module2~PrimitiveDataTypes/float.py
@@ -0,0 +1,8 @@
+float("33")
+float(33) 
+
+print(float("33"))
+print(float(33))
+
+varNumber = 123
+print(float(varNumber))
---
+++ Python/Module2~PrimitiveDataTypes/isinstance.py
@@ -0,0 +1,12 @@
+isinstance (5, int)
+isinstance (5.55, float)
+isinstance ("123", str )
+isinstance (True, bool)
+
+print( isinstance (5, int))
+print( isinstance (5.55, float))
+print( isinstance ("123", str ))
+print( isinstance (True, bool))
+
+
+
