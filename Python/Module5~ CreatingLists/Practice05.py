cbf98193c0571bc775441962b7608e0e6a0416e8
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-01 15:24:01 +0000

    Lists Ahoy!

---
+++ Python/Module5~ CreatingLists/Practice05.py
@@ -0,0 +1,15 @@
+# Create a list with 4 items
+
+shopping = ['Bread', 'Milk', 'Eggs', 'Generic']
+
+
+# Print the 1st and 3rd Items
+
+print(f"{shopping[0]}, and {shopping[2]}")
+# Prints "Bread, and Eggs"
+
+
+# Print the 2nd item while displaying its Data type
+
+print(f"{shopping[1]}, {type(shopping[1])}")
+# Prints "Milk, <class 'str'>"
