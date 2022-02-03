51435c549c0ebb72784a546002fdc1f257ffed38
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-03 14:26:09 +0000

    More Lists!

---
+++ Python/Module7~SortingandReversingLists/Practice07.py
@@ -0,0 +1,26 @@
+# Create as list - Sort it Alphabetically.
+someList = ["squash", "tomato", "bean", "corn"]
+print(someList)
+
+# 2
+sorted(someList)
+print(sorted(someList))
+
+# 1
+someList = ["squash", "tomato", "bean", "corn"]
+someList.sort()
+print(someList)
+
+
+# Create a list - Sort it Reverse Alphabetically
+anotherList = ["sssquash", "tttomato", "bbbean", "cccorn"]
+print(anotherList)
+
+# 2
+sorted(anotherList, reverse=True)
+print(sorted(anotherList, reverse=True))
+
+# 1
+anotherList = ["sssquash", "tttomato", "bbbean", "cccorn"]
+anotherList.sort(reverse=True)
+print(anotherList)
