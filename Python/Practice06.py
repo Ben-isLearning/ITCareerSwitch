411ac378c80c9f009256adb2bc477ae57cf120ae
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-03 13:55:27 +0000

    More Lists

---
+++ Python/Practice06.py
@@ -0,0 +1,38 @@
+# Create list with 5 items, print.
+# Replace 3rd item, print.
+
+shopping = ['Bread', 'Milk', 'Eggs', 'Generic', 'Specific']
+print(shopping)
+
+shopping[2] = 'Cheese'
+print(shopping)
+
+
+# Create a list with 5 items, print.
+# delete 2nd item, print.
+
+shopping = ['Bread', 'Milk', 'Eggs', 'Generic', 'Specific']
+print(shopping)
+
+del shopping[1]
+print(shopping)
+
+# Create a list with 5 items, print.
+# delete 2nd item, print.
+# 2nd Solution.
+
+shopping = ['Bread', 'Milk', 'Eggs', 'Generic', 'Specific']
+print(shopping)
+
+shopping.remove("Milk")
+print(shopping)
+
+
+# Create a list with 5 items, Print.
+# Add an item at position 3, Print.
+
+shopping = ['Bread', 'Milk', 'Eggs', 'Generic', 'Specific']
+print(shopping)
+
+shopping.insert(2, 'Toilet Paper')
+print(shopping)
