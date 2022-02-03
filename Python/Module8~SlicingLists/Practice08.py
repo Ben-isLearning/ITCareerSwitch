691c6de28ee74024a0ae64280446fd5a57b0d81d
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-03 15:10:49 +0000

    Lists!

---
+++ Python/Module8~SlicingLists/Practice08.py
@@ -0,0 +1,20 @@
+# Create a list with 8 items.
+# slice the list, omitting the first 2 items
+# and display the next 3.
+# omit = Exclude*
+
+
+createList = ["one", "two", "three", "four",
+              "five", "six", "seven", "eight", "nine", "ten"]
+
+print(createList[2:5])
+
+# Create a list with 8 items.
+# slice the list - display only odd numbers.
+
+
+createList = ["one", "two", "three", "four",
+              "five", "six", "seven", "eight", "nine", "ten"]
+
+print(createList[0::2])
+# Starting@ index 0: <No end> : Incremenet by 2
