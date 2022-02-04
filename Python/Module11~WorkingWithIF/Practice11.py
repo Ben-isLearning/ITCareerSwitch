24f49f17458065310fb7cf1d5492a7dec359a5e4
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-04 10:22:17 +0000

    If Statements

---
+++ Python/Module11~WorkingWithIF/Practice11.py
@@ -0,0 +1,38 @@
+# Create a probram using IF and ELIF to compare temperatures and print out
+# Somethingh about that specific temperature
+
+temperature = 0
+#temperature = 10
+#temperature = 15
+#temperature = 20
+#temperature = 25
+
+if temperature <= 0:
+    print("Corr- Thats Freezing!")
+
+elif temperature == 10:
+    print("Thats C-C-Cold!!")
+
+elif temperature == 15:
+    print("Thats Jacket Weather!")
+
+elif temperature == 20:
+    print("Nice!")
+
+else:
+    print("Corr- Thats Hot Hot Hot!!!")
+
+print("-----")
+# Create a probram using IF and ELIF to compare temperatures and print out
+# Somethingh about that specific temperature
+
+temp = 16
+hot = 25
+cold = 10
+
+if temp < cold:
+    print("Corr~ Its Chilly!")
+elif temp > hot:
+    print("Its Hot!")
+else:
+    print(f"It is {temp} degrees, Its quite mild!")
