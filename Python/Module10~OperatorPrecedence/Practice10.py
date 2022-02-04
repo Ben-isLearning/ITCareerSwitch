cd81a6b778e9638a2b39c66dbff63611ae562531
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-04 09:42:35 +0000

    Learning Python!

---
+++ Python/Module10~OperatorPrecedence/Practice10.py
@@ -0,0 +1,21 @@
+# Create program to test BEMDAS using arithmetic Operators
+
+x = 10
+y = 5
+z = 2
+
+# PEMDAS
+# ()
+# **
+# *
+# /
+# +
+# -
+
+print(x - y)
+
+print(x - y + z)
+
+print(y + z * x - y)
+
+print((y + z) * (x - y))
