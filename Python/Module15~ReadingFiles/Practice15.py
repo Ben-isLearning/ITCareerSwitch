6bc9a495dc48ac3309fb5976c76121c4c5d89f9c
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-07 15:52:29 +0000

    Mod 12/13/14

---
+++ Python/Module15~ReadingFiles/Practice15.py
@@ -0,0 +1,36 @@
+alpha = open("demo.txt", "r")
+print(alpha.read())
+
+print("-----------")
+
+beta = open("demo.txt", "r")
+print(beta.readline())
+beta.close()
+
+print("-----------")
+
+alpha = open("demo.txt", "r")
+print(alpha.read(24))
+
+print("-----------")
+
+with open("demo.txt") as charlie:
+    contents = charlie.read()
+    print(contents)
+
+print("-----------")
+
+delta = open("demo.txt", "r")
+print(delta.read())
+delta.close()
+print("-----------")
+delta = open("demo.txt", "a")
+delta.write("\n Its almost Lunch Time!")
+delta.close()
+print("-----------")
+delta = open("demo.txt", "r")
+print(delta.read())
+delta.close()
+
+print("-----------")
+print("-----------")
