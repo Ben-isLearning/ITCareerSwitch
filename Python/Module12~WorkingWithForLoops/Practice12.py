6981077415f0d42c68650c45153b9d2b7b0bd0cf
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-07 09:49:58 +0000

    ForLoops

---
+++ Python/Module12~WorkingWithForLoops/Practice12.py
@@ -0,0 +1,32 @@
+colors = ["red", "blue", "green"]
+
+for variable in colors:
+    print(variable)
+
+print("--------------------------")
+
+for a in range(10):
+    print(a)
+
+print("--------------------------")
+
+for b in range(5, 10):
+    print(b)
+print("--------------------------")
+
+for x in range(0, 20, 5):
+    print(x)
+print("--------------------------")
+
+# Create a loop and display your favorite foods (atleast 3) with a print statement
+
+favFoods = ["Chicken", "Vegetables", "Indian"]
+for food in favFoods:
+    print(food)
+
+print("--------------------------")
+
+# Create a loop and display all the odd numbers between 0 and 30
+
+for oddNums in range(1, 30, 2):
+    print(oddNums)
