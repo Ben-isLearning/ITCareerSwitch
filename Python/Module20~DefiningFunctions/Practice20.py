d6d554faa93d9e91187f4a0c8abbc5a29298841f
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-15 10:05:54 +0000

    Defining Functions

---
+++ Python/Module20~DefiningFunctions/Practice20.py
@@ -0,0 +1,12 @@
+def function_one():
+    print("Hello World!")
+
+
+function_one()
+
+
+def function_two(fname, lname):
+    print(f"Hello there, {fname} {lname}. How do you do?")
+
+
+function_two("Dave", "Larke")
