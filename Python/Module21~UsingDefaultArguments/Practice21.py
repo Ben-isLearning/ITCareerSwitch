6878914077984eed92c7e41bc9cf82e7993318aa
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-15 10:14:55 +0000

    Default Arguments

---
+++ Python/Module21~UsingDefaultArguments/Practice21.py
@@ -0,0 +1,9 @@
+def student(fname, lname="Bigger", major="IT"):
+    print(fname, lname, "majors in ", major)
+
+
+student("Dave")
+
+student("Don", "Oscar")
+
+student("Donny", "Essex", "Asshattery")
