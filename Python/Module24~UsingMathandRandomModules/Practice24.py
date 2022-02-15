37d49ab3dae89948c127f5d51c9e47f3ab7ecdb9
Ben-isLearning <68328490+Ben-isLearning@users.noreply.github.com>
2022-02-15 13:34:23 +0000

    Using math and random

---
+++ Python/Module24~UsingMathandRandomModules/Practice24.py
@@ -0,0 +1,20 @@
+import random
+import math
+
+# num = int(input("Give me a number, I'll square root it!: "))
+# print(math.sqrt(num))
+
+
+print("Printing random Floating Number...")
+print(random.random())
+
+print("-----------")
+
+print("Printing random Number...")
+print(random.randint(0, 100))
+# Random Integer beteen X and Y (0 and 100)
+
+
+print("Printing random Number...")
+print(random.randrange(0, 10, 2))
+# Random Integer Between X and Y - Count by Z.
